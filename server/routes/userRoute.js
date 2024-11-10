3a582309cb451d2f9cc775b91e1033d20f756137
ThomasVu <samduykhanh01@gmail.com>
2024-11-10 10:42:03 -0500

    AuthRequired done

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -8,12 +8,12 @@ const {
   updateUser
 } = require('../controllers/userController');
 
-// const requireAuth = require('../middleware/requireAuth.js') 
+const requireAuth = require('../middleware/requireAuth.js') 
 
 const router = express.Router();
 
 // Require auth for all user actions
-// router.use(requireAuth);
+router.use(requireAuth);
 
 // Define routes for user operations
 
